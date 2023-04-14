99d2b6ae518144466e769c98abca265accc2d1e0
Advait <advaitkhadye08@gmail.com>
2023-04-14 14:37:06 +0530

    initial commit

--- MyProject/MyApp/static/script.js
+++ MyProject/MyApp/static/script.js
@@ -2,11 +2,10 @@
 
     (function(d, m){
         var kommunicateSettings =
-            {"appId":"fc877d53aac4e7b9e00aa62e0672e1d0","popupWidget":true,"automaticChatOpenOnNavigation":true};
+            {"appId":"1e5fb7004ac988aca89fa3a7e0e1b616d","popupWidget":true,"automaticChatOpenOnNavigation":true};
         var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
         s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
         var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
         window.kommunicate = m; m._globals = kommunicateSettings;
     })(document, window.kommunicate || {});
 /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
-
